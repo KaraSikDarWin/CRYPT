9e17073418defd84b2c5c252546bf8e9612b58ae
PROCHILLER <78887724+KaraSikDarWin@users.noreply.github.com>
2021-11-18 16:52:51 +1000

    Add files via upload

--- RSA (2).py
+++ RSA (2).py
@@ -1,9 +1,50 @@
 import random, math
+
+
+
 E = []
 D = []
 d=0
-p = 13
-q = 17
+
+
+prime1 = True
+prime2 =True
+while prime1:
+    k=2
+    q=random.randint(1,1000)
+    if q > 1:
+        for i in range(2, int(q/2)+1):
+            if (q%i)==0:
+                break
+        else:
+            prime1=False
+    else:
+        pass
+
+while prime2:
+    k=2
+    p=random.randint(1,1000)
+    if p > 1:
+        for i in range(2, int(p/2)+1):
+            if (p%i)==0:
+                break
+        else:
+            prime2=False
+    else:
+        pass
+
+print(p,q)
+#     if q % 2==1 and p % 2==1:
+#
+#
+#     else:
+#         if p %2 ==0:
+#
+#             q = random.randint(1, 10000)
+#         else:
+#             p= random.randint(1,10000)
+
+
 n = p * q
 fi = (p - 1) * (q - 1)
 
@@ -27,7 +68,10 @@ def gcd_extended(num1, num2):
     return (div, y - (num2 // num1) * x, x)
 D = gcd_extended(e, fi)
 
+
 d=D[1]
+if d<0:
+    d=d %fi
 print("d - " +str(d))
 
 
@@ -41,3 +85,8 @@ print(f'Coder - {zakodtext}')
 
 iznachtext = (zakodtext ** privateKey[1])%privateKey[0]
 print(f'Message - {iznachtext}')
+
+print("PODPIS")
+S=(m**d)%n
+if (S** e) %n == m:
+    print("YEah")
